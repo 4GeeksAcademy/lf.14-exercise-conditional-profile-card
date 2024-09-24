dcff9b6432257712ee2077fed0952032e1d4cb82
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-24 14:10:52 +0000

    [Agrego template literal a las RRSS también]
    app.js-se incorpora la funcionalidad a los enlaces de redes sociales para que,
    si no se ingresa nada, deriven a web general de cada una, y si se ingresa username derive al perfil.-

--- src/js/app.js
+++ src/js/app.js
@@ -40,16 +40,48 @@ function render(variables = {}) {
           <h3>${variables.city == null ? "Miami," : variables.city} ${
     variables.country == null ? "USA" : variables.country
   }</h3>
-          <ul class="${
-            variables.socialMediaPosition == "position-right"
-              ? "position-right"
-              : variables.socialMediaPosition
-          }">
-            <li><a href="https://https://x.com/"><i class="fa-brands fa-x-twitter"></i></a></li>
-            <li><a href="https://github.com"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+   <ul class="${
+     variables.socialMediaPosition == "position-right"
+       ? "position-right"
+       : variables.socialMediaPosition
+   }">
+  <li>
+    <a href="${
+      variables.twitter
+        ? `https://twitter.com/${variables.twitter}`
+        : "https://x.com/"
+    }">
+      <i class="fa-brands fa-x-twitter"></i>
+    </a>
+  </li>
+  <li>
+    <a href="${
+      variables.github
+        ? `https://github.com/${variables.github}`
+        : "https://github.com/"
+    }">
+      <i class="fab fa-github"></i>
+    </a>
+  </li>
+  <li>
+    <a href="${
+      variables.linkedin
+        ? `https://linkedin.com/in/${variables.linkedin}`
+        : "https://linkedin.com/"
+    }">
+      <i class="fab fa-linkedin"></i>
+    </a>
+  </li>
+  <li>
+    <a href="${
+      variables.instagram
+        ? `https://instagram.com/${variables.instagram}`
+        : "https://instagram.com/"
+    }">
+      <i class="fab fa-instagram"></i>
+    </a>
+  </li>
+</ul>
         </div>
     `;
 }
